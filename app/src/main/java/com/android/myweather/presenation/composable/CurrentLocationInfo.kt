d6db6f1c711bcb004eecb5ce63213a7a6c98acab
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-06-09 10:36:51 +0300

    implement current location info design

---
+++ app/src/main/java/com/android/myweather/presenation/composable/CurrentLocationInfo.kt
@@ -0,0 +1,50 @@
+package com.android.myweather.presenation.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.android.myweather.R
+import com.android.myweather.ui.theme.darkRed
+import com.android.myweather.ui.theme.letterSpacing25
+import com.android.myweather.ui.theme.text16
+import com.android.myweather.ui.theme.urbanist
+
+@Composable
+fun CurrentLocationInfo(){
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    ){
+        Icon(
+            painter = painterResource(R.drawable.location),
+            contentDescription = null,
+            tint = darkRed,
+            modifier = Modifier.size(24.dp)
+        )
+        Text(
+            text = "Baghdad" ,
+            color = darkRed,
+            fontSize = text16,
+            letterSpacing = letterSpacing25,
+            textAlign = TextAlign.Start,
+            fontWeight = FontWeight.W500,
+            fontFamily = urbanist,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CurrentLocationInfoPreview(){
+    CurrentLocationInfo()
+}
--- app/src/main/java/com/android/myweather/ui/theme/Color.kt
+++ app/src/main/java/com/android/myweather/ui/theme/Color.kt
@@ -13,6 +13,7 @@ val Pink40 = Color(0xFF7D5260)
 val lightBlue = Color(0xFF87CEFA)
 val darkBlue = Color(0xFF060414)
 val Blue32pre = Color(0x5200619D)
+val darkRed = Color(0xFF323232)
 
 val darkBlue60pre = Color(0x99060414)
 val darkBlue8pre = Color(0x14060414)
