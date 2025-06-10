4323c765a397a65f1a77db23a14ce1a27d62f15e
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-06-10 14:04:23 +0300

    add dependency inversion

--- app/src/main/java/com/android/myweather/MainActivity.kt
+++ app/src/main/java/com/android/myweather/MainActivity.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.android.myweather.di.appModule
 import com.android.myweather.presenation.screen.HomeScreen
+import com.android.myweather.presenation.screen.testScreen
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startKoin {
+            androidContext(this@MainActivity)
+            modules(appModule)
+        }
         enableEdgeToEdge()
         setContent {
-            HomeScreen()
+            //HomeScreen()
+            testScreen()
         }
     }
 }
--- app/src/main/java/com/android/myweather/di/AppModule.kt
+++ app/src/main/java/com/android/myweather/di/AppModule.kt
@@ -1,2 +1,45 @@
 package com.android.myweather.di
 
+import com.android.myweather.data.remote.mapper.WeatherMapper
+import com.android.myweather.data.remote.repository.LocationRepositoryImpl
+import com.android.myweather.data.remote.repository.WeatherRepositoryImpl
+import com.android.myweather.domain.repository.LocationRepository
+import com.android.myweather.domain.repository.WeatherRepository
+import com.android.myweather.domain.usecase.GetCurrentLocationUseCase
+import com.android.myweather.domain.usecase.GetCurrentWeatherUseCase
+import com.android.myweather.presenation.viewmodel.WeatherViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+    single<FusedLocationProviderClient> {
+        LocationServices.getFusedLocationProviderClient(androidContext())
+    }
+    //single<HttpClient> { HttpClient(CIO) }
+    single<LocationRepository> {
+        LocationRepositoryImpl(
+            fusedLocationProviderClient = get(),
+            context = androidContext()
+        )
+    }
+    single<WeatherRepository> {
+        WeatherRepositoryImpl(
+            weatherMapper = get()
+        )
+    }
+    single { WeatherMapper() }
+    single { GetCurrentLocationUseCase(get()) }
+    single { GetCurrentWeatherUseCase(get()) }
+    viewModel {
+        WeatherViewModel(
+            getCurrentWeatherUseCase = get(),
+            getCurrentLocationUseCase = get(),
+            context = androidContext()
+        )
+    }
+}
+
